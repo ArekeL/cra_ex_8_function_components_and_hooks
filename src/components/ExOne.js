caf0b4309385a198939c4675279bb92f4132b6ed
Arek Lechnik <alechnik@gmail.com>
2023-01-25 20:00:31 +0100

    Exercise One Shopping list - sfc + hooks (state)

--- src/components/ExOne.js
+++ src/components/ExOne.js
@@ -1,11 +1,31 @@
+import React from "react";
 import "./App.css";
-const ExOne = () => {
+
+const ExOne = (props) => {
+	const [items] = React.useState({
+		item1: "Marchewka",
+		item2: "Woda",
+		item3: "Ziemniak",
+	});
+
+	const ItemList = ({ name, example }) => (
+		<li>
+			{name} - {example}
+		</li>
+	);
+
 	return (
 		<div className='ex'>
-			<h2>Execise One</h2>
-			<div>Ex content</div>
+			<h2>Exercise One</h2>
+			<div>
+				<h4>Shopping list - sfc + hooks (state)</h4>
+				<ul>
+					<ItemList name={items.item1} example={1 + "kg"} />
+					<ItemList name={items.item2} example={2} />
+					<ItemList name={items.item3} example={4 + "kg"} />
+				</ul>
+			</div>
 		</div>
 	);
 };
-
 export default ExOne;
