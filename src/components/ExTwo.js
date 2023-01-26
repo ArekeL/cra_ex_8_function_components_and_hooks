2ad7b26e25026442beac9f37f6ff16908414c1c6
Arek Lechnik <alechnik@gmail.com>
2023-01-26 06:49:42 +0100

    Execise Two Add letter A - sfc + hooks

--- src/components/ExTwo.js
+++ src/components/ExTwo.js
@@ -1,9 +1,19 @@
+import React from "react";
 import "./App.css";
 const ExTwo = () => {
+	const [text, setText] = React.useState("");
+
+	const handleOnClick = () => {
+		const letter = "a";
+		setText(text + letter);
+	};
 	return (
 		<div className='ex'>
 			<h2>Execise Two</h2>
-			<div>Ex content</div>
+			<h4>Add letter A - sfc + hooks</h4>
+			<br />
+			<button onClick={handleOnClick}>Add Letter A</button>
+			<h4>{text}</h4>
 		</div>
 	);
 };
